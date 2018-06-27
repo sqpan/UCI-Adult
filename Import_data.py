257fbfb99aa7751aa08f5a034d62f3f63100fc8e
t213nk <shaoqing.pan89@gmail.com>
2018-06-27 12:29:41 -0700

    clean missing value

--- Import_data.py
+++ Import_data.py
@@ -9,7 +9,17 @@ import pandas as pd
 import numpy as np
 
 # import file
-file = 'adult_data.csv'
-df = pd.read_csv(file, header = None)
+file = 'no_questionMark.csv'
+df = pd.read_csv(file)
+print(df.head(10))
+print(df.shape)
+# origin dataframe shape (32561, 15)
 
-print(df.head())
+# clean miss value row 
+df_dropNull = df.dropna()
+# clean useless column (Maybe)
+#df_short = df_dropNull.drop(['], axis = 1)
+print(df_dropNull.shape)
+print(df_dropNull.head(10))
+# dataframe shape after take out row with missing value (32561, 15)
+
